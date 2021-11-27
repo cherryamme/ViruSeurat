e41e24410eda006f45db7cab9fab93f7a57dc7ee
cherryamme <cherryamme@163.com>
2021-11-27 14:40:57 +0000

    testdata_golobalchange

--- R/Input_testdata.R
+++ R/Input_testdata.R
@@ -1,11 +1,14 @@
 #' It will generat a Test Data
 #'
+#' @param pos enviroment position
+#'
 #' @export
 #'
 #'
-Input_Testdata <- function() {
+#'
+Input_Testdata <- function(pos=1) {
   Testdata <- paste0(system.file(package = "ViruSeurat"), "/testdata")
-  assign(Testmeta,read.csv("sample_info.tsv", sep = "\t", header = F, col.names = c("RAW_data", "RAW_meta"), stringsAsFactors = F),envir = .GlobalEnv)
-  assign(TestSclist,Input_Sclist(meta = Testmeta, inputfiledir = Testdata),envir = .GlobalEnv)
-  assign(TestVclist,Input_Vclist(inputfiledir = Testdata),envir = .GlobalEnv)
+  assign('Testmeta',read.csv(paste0(Testdata,"/sample_info.tsv"), sep = "\t", header = F, col.names = c("RAW_data", "RAW_meta"), stringsAsFactors = F),envir = as.environment(pos))
+  assign('TestSclist',Input_Sclist(meta = Testmeta, inputfiledir = Testdata),envir = as.environment(pos))
+  assign('TestVclist',Input_Vclist(inputfiledir = Testdata),envir = as.environment(pos))
 }
