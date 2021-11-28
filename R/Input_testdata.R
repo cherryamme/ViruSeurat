bce27025ba86b10152a89fbb28f24b9beb44c66c
cherryamme <cherryamme@163.com>
2021-11-28 04:15:33 +0000

    InputTestdata

--- R/Input_testdata.R
+++ R/Input_testdata.R
@@ -7,8 +7,8 @@
 #'
 #'
 Input_Testdata <- function(pos = 1) {
-  Testdata <- paste0(system.file(package = "ViruSeurat"), "/testdata")
-  assign("Testmeta", read.csv(paste0(Testdata, "/sample_info.tsv"), sep = "\t", header = F, col.names = c("RAW_data", "RAW_meta"), stringsAsFactors = F), envir = as.environment(pos))
+  Testdata <- paste0(system.file(package = "ViruSeurat"), "/testdata/")
+  assign("Testmeta", read.csv(paste0(Testdata, "sample_info.tsv"), sep = "\t", header = F, col.names = c("RAW_data", "RAW_meta"), stringsAsFactors = F), envir = as.environment(pos))
   assign("TestSclist", Input_Sclist(meta = Testmeta, inputfiledir = Testdata), envir = as.environment(pos))
   assign("TestVclist", Input_Vclist(inputfiledir = Testdata), envir = as.environment(pos))
 }
